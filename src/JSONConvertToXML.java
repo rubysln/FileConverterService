0f24fd0c3329dc91b4578d0a3de1518b96146952
rubysln <prostosekundu48@gmail.com>
2022-11-24 22:09:52 +0300

    Добавлен способ сохранения данных

--- src/JSONConvertToXML.java
+++ src/JSONConvertToXML.java
@@ -1,12 +1,15 @@
 import org.json.JSONObject;
+import org.json.XML;
 
 import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
+import java.nio.file.Paths;
 
 public class JSONConvertToXML {
     public static void doConvert(File file, String output) throws IOException {
         String jsonString = Files.readString(file.toPath());
         JSONObject json = new JSONObject(jsonString);
+        Files.write(Paths.get(output + ".xml"), XML.toString(json).getBytes());
     }
 }
