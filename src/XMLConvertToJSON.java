946fc20f7420e4654a6c2e75d57682615cd10f5f
rubysln <prostosekundu48@gmail.com>
2022-11-24 21:58:00 +0300

    Добавлен способ получения данных из XML-файла

--- src/XMLConvertToJSON.java
+++ src/XMLConvertToJSON.java
@@ -1,7 +1,9 @@
 import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
 
 public class XMLConvertToJSON {
-    public static void doConvert(File file,String output){
-        
+    public static void doConvert(File file,String output) throws IOException {
+        String xml = Files.readString(file.toPath());
     }
 }
