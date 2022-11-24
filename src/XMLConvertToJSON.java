bad4fc4da8adf49109cf20b33e0a421924820f82
rubysln <prostosekundu48@gmail.com>
2022-11-24 21:59:04 +0300

    Добавлен способ преобразования XML-данных в JSON-данные

--- src/XMLConvertToJSON.java
+++ src/XMLConvertToJSON.java
@@ -1,3 +1,6 @@
+import org.json.JSONObject;
+import org.json.XML;
+
 import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
@@ -5,5 +8,6 @@ import java.nio.file.Files;
 public class XMLConvertToJSON {
     public static void doConvert(File file,String output) throws IOException {
         String xml = Files.readString(file.toPath());
+        JSONObject jsonObject = XML.toJSONObject(xml);
     }
 }
