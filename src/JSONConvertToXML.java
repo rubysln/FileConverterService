89ee52ff9014db23e491394fe71678ceca8440dc
rubysln <prostosekundu48@gmail.com>
2022-11-24 22:07:49 +0300

    Добавлен способ получения данных из JSON-файла

--- src/JSONConvertToXML.java
+++ src/JSONConvertToXML.java
@@ -1,7 +1,9 @@
 import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
 
 public class JSONConvertToXML {
-    public static void doConvert(File file, String output){
-        
+    public static void doConvert(File file, String output) throws IOException {
+        String jsonString = Files.readString(file.toPath());
     }
 }
